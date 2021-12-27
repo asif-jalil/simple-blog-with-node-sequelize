5f6a78d541c27c912c61a844d3d3bded49b72bfb
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2021-12-28 01:38:36 +0600

    get post fixed

--- src/posts/posts.service.js
+++ src/posts/posts.service.js
@@ -1,31 +1,31 @@
-const db = require('../db/db');
+const db = require("../db/db");
 
 // create main model
 const Post = db.post;
 const User = db.user;
 
 module.exports.createPost = async function (post) {
-	return await Post.create(post);
+  return await Post.create(post);
 };
 
 module.exports.getPosts = async function () {
-	return await Post.findAll({
-		include: [User]
-	});
+  return await Post.findAll({
+    include: [{ model: User, attributes: ["id", "name"] }],
+  });
 };
 
 module.exports.getPostById = async function (postId) {
-	return await Post.findOne({ where: { id: postId } });
+  return await Post.findOne({ where: { id: postId } });
 };
 
 module.exports.updatePostById = async function (postId, update) {
-	return await Post.update(update, { where: { id: postId } });
+  return await Post.update(update, { where: { id: postId } });
 };
 
 module.exports.deletePostById = async function (postId) {
-	return await Post.destroy({ where: { id: postId } });
+  return await Post.destroy({ where: { id: postId } });
 };
 
 module.exports.getPostByPagination = async function (offset, limit) {
-	return await Post.findAll({ offset: offset, limit: limit })
-}
+  return await Post.findAll({ offset: offset, limit: limit });
+};
